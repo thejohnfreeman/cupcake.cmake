505565e18ac307929f4a94827eb9c14e8fc4cd31
John Freeman <jfreeman08@gmail.com>
2023-11-24 13:49:20 -0600

    Bump version to 0.4.2

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMake, CMakeToolchain
 
 class Cupcake(ConanFile):
     name = 'cupcake'
-    version = '0.4.1'
+    version = '0.4.2'
     default_user = 'github'
     default_channel = 'thejohnfreeman'
 
