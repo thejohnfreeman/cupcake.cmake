869d11338f39c957f5a2c58f2b4cb8e0ca8a6da3
John Freeman <jfreeman08@gmail.com>
2024-10-18 11:38:29 -0500

    Echo execute commands

--- cmake/data/call.cmake
+++ cmake/data/call.cmake
@@ -1,2 +1,14 @@
 # Called by `execute.<name>` custom targets.
+# TODO: test no arguments
+# TODO: test with arguments containing ';'
+# https://cmake.org/cmake/help/latest/command/string.html#join
+# TODO: test with arguments containing '''
+string(JOIN "' '" arguments $ENV{CUPCAKE_EXE_ARGUMENTS})
+if(arguments)
+  set(arguments " '${arguments}'")
+endif()
+# Start with a carriage return to overwrite the prefix added by CMake.
+# https://stackoverflow.com/a/71869855/618906
+string(ASCII 13 CR)
+message(STATUS "${CR}${executable}${arguments}")
 execute_process(COMMAND "${executable}" $ENV{CUPCAKE_EXE_ARGUMENTS})
